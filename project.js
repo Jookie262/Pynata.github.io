76c25be5c19a723e3552ebe932f75981c387566e
Laikaiyong <vandycklai@gmail.com>
2021-08-13 21:46:52 +0800

    Reformat and edit project content

--- project.js
+++ project.js
@@ -5,8 +5,8 @@ let project_list = [
     {
         Name: 'Youtube Converter',
         ModalColor: 'White',
-        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-        Version: "v.1.20",
+        Description: "A online website that can convert your desired youtube link to mp3/mp4 file for download usage.",
+        Version: "-",
         Link: 'https://github.com/Pynata/02-Youtube-Converter',
         Image: 'https://cdn.dribbble.com/users/2939235/screenshots/7085112/media/5a60937babf66a0001ea7a7de8707407.png?compress=1&resize=1200x900',
         Badges: [{
@@ -28,8 +28,8 @@ let project_list = [
     {
         Name: 'Van Paradise',
         ModalColor: 'Black',
-        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-        Version: "v.1.20",
+        Description: "A discord bot that is capable for music playing, levelling system and mini game",
+        Version: "v.3.1",
         Link: 'https://github.com/Laikaiyong/Van-Paradise',
         Image: 'https://cdn.dribbble.com/users/281679/screenshots/14897126/media/f52c47307ac2daa0c727b1840c41d5ab.png?compress=1&resize=1000x750',
         Badges: [{
@@ -51,8 +51,8 @@ let project_list = [
     {
         Name: 'KYGorGor Bot',
         ModalColor: 'white',
-        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-        Version: "v.1.20",
+        Description: "A discord bot that act as butler bot (welcoming message, server description)",
+        Version: "v.6.1",
         Link: 'https://github.com/Laikaiyong/KY-Gor-Gor-Bot',
         Image: 'https://cdn.dribbble.com/users/1069040/screenshots/13871963/media/37eef1639e0e048d8f28d7fd65bdd503.png?compress=1&resize=800x600',
         Badges: [{
@@ -74,8 +74,8 @@ let project_list = [
     {
         Name: 'RPG Game',
         ModalColor: 'Black',
-        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-        Version: "v.1.20",
+        Description: "RPG Game (Idea generating)",
+        Version: "-",
         Link: 'https://github.com/Pynata/04-RPG-Game',
         Image: 'https://cdn.dribbble.com/users/989466/screenshots/6639761/canopy-two-dots-rpg-map-drib-crop-volcanic-_2x_4x.png?compress=1&resize=1200x900',
         Badges: [{
@@ -92,8 +92,8 @@ let project_list = [
     {
         Name: 'Ads Blocker',
         ModalColor: 'Black',
-        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-        Version: "v.1.20",
+        Description: "Free Ads blocker extension for Chrome Browser. ",
+        Version: "-",
         Link: 'https://github.com/Pynata/03-Ads-Blocker',
         Image: 'https://getadblock.com/images/platforms/firefox/firefox_hero_image_exportable.svg?v=da2b0808',
         Badges: [{
@@ -110,8 +110,8 @@ let project_list = [
     {
         Name: 'Social Media App',
         ModalColor: 'white',
-        Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-        Version: "v.1.20",
+        Description: "Social media app (Idea generating)",
+        Version: "-",
         Link: 'https://github.com/Pynata/05-Social-Media-App',
         Image: 'https://cdn.dribbble.com/users/2510246/screenshots/5089720/illustration.png?compress=1&resize=800x600',
         Badges: [{
